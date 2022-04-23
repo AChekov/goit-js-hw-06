aaa280f53c871725c7fa7ae073127706efea0723
AChekov <4ekoff@gmail.com>
2022-04-23 21:45:47 +0300

    group 45

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -15,13 +15,13 @@ const images = [
 
 const galleryItemEl = document.querySelector(".gallery");
 
-// const makeGallery = images
-//   .map(
-//     ({ url, alt }) =>
-//       `<li class="gallery__item"><img src="${url}" alt="${alt}" class="gallery__image"/></li>`
-//   )
-//   .join("");
+const makeGallery = images
+  .map(
+    ({ url, alt }) =>
+      `<li class="gallery__item"><img src="${url}" alt="${alt}" class="gallery__image"/></li>`
+  )
+  .join("");
 
-// galleryItemEl.insertAdjacentHTML("afterbegin", makeGallery);
+galleryItemEl.insertAdjacentHTML("afterbegin", makeGallery);
 
-// galleryItemEl.classList.add("gallery__list", "list");
+galleryItemEl.classList.add("gallery__list", "list");
